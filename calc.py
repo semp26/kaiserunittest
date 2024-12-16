5ba8eae453bbd12ccf14f106961b1884ab44421b
Max Burkert <burkert.max@gmail.com>
2024-12-16 13:42:29 +0100

    Test erstellt

--- calc.py
+++ calc.py
@@ -12,5 +12,17 @@ def calculator(num1, num2, operator):
     else:
         return 'Invalid operator'
         
+def test_add():
+    assert calculator(1, 2, '+') == 3
 
+def test_sub():
+    assert calculator(2, 1, '-') == 1
 
+def test_mult():
+    assert calculator(2, 2, '*') == 4
+
+def test_div():
+    assert calculator(2, 1, '/') == 2
+
+def test_fail()
+    assert calculator() == 'Invalid operator'
